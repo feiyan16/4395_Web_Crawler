7eb71df8bda472868b7e6da3d8a9c82f4618873a
Feiyan <feiyan.su16@gmail.com>
2022-03-24 17:53:21 -0500

    Commented Everything

--- crawler.py
+++ crawler.py
@@ -1,15 +1,17 @@
+from scrapy.exceptions import CloseSpider
+
 import sys_color as write
 import scrapy
-import sys
 
 
 class RedditSpider(scrapy.Spider):
     name = 'Reddit-Movies'
+    # START_URLS 1:
     # start_urls = ['https://old.reddit.com/r/movies/']
-    start_urls = ['https://old.reddit.com/search?q=the+batman&count=22&after=t3_tjkf1z']
-    counter = 0
-    urls = []
+    start_urls = ['https://old.reddit.com/search?q=the+batman&count=22&after=t3_tjkf1z']  # URL page to start with
+    counter = 0  # relevant link counter, used to exit once it hits 20
 
+    # FOR START_URLS 1:
     # def parse(self, response, **kwargs):
     #     links = [a for a in response.xpath('//a[contains(@data-event-action, "title")]/@href').getall()]
     #     for url in links:
@@ -20,42 +22,46 @@ class RedditSpider(scrapy.Spider):
 
     def parse(self, response, **kwargs):
         write.stdout(f'current page {response.url}')
+        # gets list of links that are in the current url page
         links = [div for div in response.xpath('//div/div/a[contains(@class, "search-link may-blank")]/@href').getall()]
+        # FILTER THE URLS:
+        # URLs must not belong in reddit domain, not twitter because twitter doesn't
+        # allow scraping, and url must not be a link to an image, because then there is not text to parse
         for url in links:
             if 'redd.it' not in url and 'reddit' not in url and 'twitter' not in url:
                 if '.png' not in url and '.gif' not in url and '.jpg' not in url:
-                    yield scrapy.Request(url=url, callback=self.parse_page)
+                    yield scrapy.Request(url=url, callback=self.parse_page)  # parse html for url page
 
-        yield from self.go_to_next_page(response)
+        yield from self.go_to_next_page(response)  # go to the next page in the reddit forum/discussion
 
     def go_to_next_page(self, response):
+        # Get the url for the next page by getting the href attribute value from the next page html button
         next_page = response.xpath('//a[contains(@rel, "nofollow next")]/@href').get()
-        if next_page:
+        if next_page:  # if there is a next page
             write.stdout(f'Going to next page {next_page}')
-            yield scrapy.Request(url=next_page, callback=self.parse)
+            yield scrapy.Request(url=next_page, callback=self.parse)  # follow and call self.parse on the url again
         else:
             write.stderr('No Next Page')
 
     def parse_page(self, response):
-        if self.counter == 20:  # stopping condition, kills remaining threads
-            write.stderr(f'link count = {self.counter}')
-            sys.exit(0)
+        if self.counter == 20:  # stopping condition, stops web crawler
+            raise CloseSpider(f'link count = {self.counter}')
         write.stdout(f'Parsing {response.url}...')
-        texts = self.scrape(response)  # scrape for texts on page
-        if texts:
+        body = self.scrape(response)  # scrape for texts on page
+        if body:  # if body is not empty
             filename = f'scraped_texts/url_{self.counter}.txt'
             file = open(filename, 'w')
-            file.write(texts)
+            file.write(body)  # write body out to a file
             write.stdok(f'SUCCESS: {response.url} written successfully as {filename}')
 
     def scrape(self, response):
         body = ''
-        texts = [t for t in response.xpath('//title/text()').getall()]
-        texts += [p for p in response.xpath('//p/text()').getall()]
-        if len(texts) >= 10:  # Minimum amount of information needed
+        texts = [t for t in response.xpath('//title/text()').getall()]  # get the title of the url page
+        texts += [p for p in response.xpath('//p/text()').getall()]  # get all texts from the HTML paragraph element
+        if len(texts) >= 10:  # Minimum amount of information needed on url page
             for text in texts:
                 body += (text + '\n')
-            self.counter += 1
+            self.counter += 1  # increment relevant link counter
         else:
             write.stdwarn(f'{response.url} does not have enough data')
         return body
--- main.py
+++ main.py
@@ -1,122 +1,185 @@
-import crawler
+
+import crawler as Crawler
+from scrapy.crawler import CrawlerProcess
 import os
 import re
 import nltk
-import math
 import csv
 from nltk import WordNetLemmatizer
 from nltk.corpus import stopwords
-from scrapy.crawler import CrawlerProcess
-
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
+# PROMPT 3:
 def output_sentences(filename):
     file = open(filename, 'r')
+    topic = file.readline()  # Get the title of the URL page
     text = [(t.strip() + '\n') for t in file.readlines() if t.strip()]  # remove leading and trailing spaces
-    sentences = nltk.sent_tokenize(''.join(text))
-    number = filename.split('/')[1].split('_')[1]
-    file = open(f'processed_texts/text_{number}', 'a')
+    sentences = nltk.sent_tokenize(''.join(text))  # break into sentences
+    number = filename.split('/')[1].split('_')[1]  # get the x.txt part of url_x.txt
+    file = open(f'processed_texts/text_{number}', 'a')  # open text file
     for sentence in set(sentences):
-        file.write(sentence + '\n')
+        file.write(sentence + '\n')  # append sentences to the file
+    return topic
 
 
+# NORMALIZATION
 def process_sentence(sentence):
     tokens = []
-    sentence = sentence.lower().strip()
-    sentence = re.sub('[\\d]', '', sentence)
-    sentence = re.sub(r'[^\w\s]', ' ', sentence)
-    for t in nltk.word_tokenize(sentence):
+    sentence = sentence.lower().strip()  # make lowercase and remove leading and trailing spaces
+    sentence = re.sub('[\\d]', '', sentence)  # remove digits
+    sentence = re.sub(r'[^\w\s]', ' ', sentence)  # remove all whitespaces that are not spaces
+    for t in nltk.word_tokenize(sentence):  # for each word in the sentence
+        # if token is not empty & token is a word & token is not a stopword & token has > 3 characters
         if t and t.isalpha and t not in stopwords.words('english') and len(t) > 3:
             tokens.append(t + ' ')
-    return ''.join(set(lemmatize(tokens)))
+    return ''.join(set(lemmatize(tokens)))  # lemmatize tokens and join it back into a sentence
 
 
+# LEMMATIZATION
 def lemmatize(tkns):
     wnl = WordNetLemmatizer()
     lemmas = [wnl.lemmatize(t) for t in tkns]
     return lemmas
 
 
-def make_data(filename):
+# CREATE DOCUMENTS FOR CORPUS
+def make_docx(filename):
     file = open(f'processed_texts/{filename}', 'r')
     sentences = []
-    for s in file.readlines():
-        sentence = process_sentence(s).strip()
-        if sentence:
-            sentences.append(sentence + ' ')
-    return ''.join(sentences)
+    for s in file.readlines():  # for every sentence
+        sentence = process_sentence(s).strip()  # process the sentence
+        if sentence:  # if sentences is not empty
+            sentences.append(sentence + ' ')  # append into array
+    return ''.join(sentences)  # join it back into a document
 
 
-def get_tf(doc):
-    tokens = nltk.word_tokenize(doc)
-    token_set = set(tokens)
-    tf_dict = {t: tokens.count(t) for t in token_set}
-    for t in tf_dict.keys():
-        tf_dict[t] = tf_dict[t] / len(tokens)
-    return tf_dict
+# def get_tf(doc):
+#     tokens = nltk.word_tokenize(doc)
+#     token_set = set(tokens)
+#     tf_dict = {t: tokens.count(t) for t in token_set}
+#     for t in tf_dict.keys():
+#         tf_dict[t] = tf_dict[t] / len(tokens)
+#     return tf_dict
+#
+#
+# def get_idf(docs, tf_s):
+#     idf_dict = {}
+#     vocab = []
+#     for key in [list(tf.keys()) for tf in tf_s]:
+#         vocab += key
+#     for word in vocab:
+#         count = ['x' for doc in docs if word in doc]
+#         idf_dict[word] = math.log((1 + len(docs)) / (1 + len(count)))
+#     return idf_dict
+#
+#
+# def get_tfidf(tf, idf):
+#     tf_idf = {}
+#     for t in tf.keys():
+#         tf_idf[t] = tf[t] * idf[t]
+#
+#     return tf_idf
 
 
-def get_idf(docs, tf_s):
-    idf_dict = {}
-    vocab = []
-    for key in [list(tf.keys()) for tf in tf_s]:
-        vocab += key
-    for word in vocab:
-        count = ['x' for doc in docs if word in doc]
-        idf_dict[word] = math.log((1 + len(docs)) / (1 + len(count)))
-    return idf_dict
+# PROMPT 4
+def get_top40_keywords(matrix, tokens, doc_no):
+    all_values = {}
+    for n in range(len(matrix.toarray())):  # loop through each row of tf-idf values
+        tfidf_values = matrix.toarray()[n]  # get the row
+        for idx in range(len(tfidf_values)):
+            word = tokens[idx]  # get the corresponding word at the index
+            value = tfidf_values[idx]
+            if word in all_values:
+                if value > all_values[word][1]:  # if new value is greater than old
+                    all_values[word] = (doc_no[n], value)  # use new value
+            else:
+                all_values[word] = (doc_no[n], value)
+    return sorted(all_values.items(), key=lambda x: x[1][1], reverse=True)  # return in descending order
 
 
-def get_tfidf(tf, idf):
-    tf_idf = {}
-    for t in tf.keys():
-        tf_idf[t] = tf[t] * idf[t]
+# FIND THE TOP 25 TERMS FOR EACH URL PAGE: Used to decide the top 10 terms in the top 40 terms
+def keywords_by_topic(matrix, topics, doc_no):
+    rows = []  # rows to write out to csv
+    for n in range(len(matrix.toarray())):  # loop through each row of tf-idf values
+        idx = doc_no[n]  # find the x in url_x.txt that n is pointing to
+        topic = topics.get(idx)  # get the topic for url_x.txt
 
-    return tf_idf
+        top_25 = {}  # to hold the top 25 word, which url it came from, and it's tf-idf number
+        tfidf_values = matrix.toarray()[n]
+        top_25_values = sorted(tfidf_values, reverse=True)[:25]  # top 25 values for the row in matrix
+        for i in range(len(tfidf_values)):  # loop through whole row
+            value = tfidf_values[i]  # get value
+            if value in top_25_values:  # if value is in the list of top 25
+                top_25[tfidf_tokens[i]] = value  # get word at index i and store as {'word': tf-idf val}
+        # sort the dictionary according to value, ('word1', 0.2), ('word2', 0.1)...
+        sorted_top25 = sorted(top_25.items(), key=lambda x: x[1], reverse=True)  # sort the dictionary
+        word = [k[0] for k in sorted_top25]  # get the words from sorted list
+        values = [k[1] for k in sorted_top25]  # get the values from sorted list
+        # OPTIONAL #
+        rows.append([idx, topic])
+        rows.append(word)
+        rows.append(values)
+        rows.append(['\n'])
+        # OPTIONAL #
+    return rows
 
 
 if __name__ == "__main__":
-    process = CrawlerProcess()
-    process.crawl(crawler.RedditSpider)
-    process.start()
+    # NOTE:
+    # If you run WEB CRAWLER again, it will pull a different set of urls and ultimately
+    # will cause a different list of top 40 terms, which means you will have to re-pick the top 10 terms from those
+    # 40 terms, and recreate the knowledge base... SO. Proceed with CAUTION
 
-    for name in os.listdir('scraped_texts'):
-        output_sentences(f'scraped_texts/{name}')
+    # WEB CRAWLER:
+    # process = CrawlerProcess()
+    # process.crawl(Crawler.RedditSpider)
+    # process.start()
 
-    documents = []
-    tf_set = []
-    doc_names = []
-    for name in os.listdir('processed_texts'):
-        docx = make_data(name)
-        doc_names.append(name)
-        documents.append(docx)
-        # term_freq = get_tf(docx)
-        # tf_set.append(term_freq)
+    # CREATE PROCESSED_TEXTS:
+    titles = {}  # e.g. {14: 'url title'} = title of url_14.txt is 'url title'
+    for name in os.listdir('scraped_texts'):  # loops through the files in directory
+        title = output_sentences(f'scraped_texts/{name}')  # create sentences from url page & return the title of page
+        index = int(name.split('_')[1].split('.')[0]) - 1  # get the x part of url-x.txt
+        titles[index] = title.replace('\n', '')  # remove newline and store
 
+    # CREATE CORPUS:
+    corpus = []  # holds documents made from url pages
+    doc_index = []  # holds indices of documents, IMPT: requires order
+    for name in os.listdir('processed_texts'):  # loops through the files in directory
+        docx = make_docx(name)  # process sentences and make documents to create corpus
+        doc_index.append(int(name.split('_')[1].split('.')[0]) - 1)  # get the x part of url-x.txt
+        corpus.append(docx)
+
+    # CREATE TF-IDF VALUES:
     tfidfvectorizer = TfidfVectorizer(ngram_range=(1, 2), max_df=0.95, min_df=2)
-    tfidf_matrix = tfidfvectorizer.fit_transform(documents)
+    tfidf_matrix = tfidfvectorizer.fit_transform(corpus)
     tfidf_tokens = tfidfvectorizer.get_feature_names_out()
-    # tfidf_vect = pd.DataFrame(data=tfidf_matrix.toarray(), index=doc_names, columns=tfidf_tokens)
 
-    rows = []
-    for n in range(len(tfidf_matrix.toarray())):
-        tfidf_values = tfidf_matrix.toarray()[n]
-        top_25 = {}
-        top_25_values = sorted(tfidf_values, reverse=True)[:25]
-        rows.append([f'DOCUMENT: {doc_names[n]}'])
-        for i in range(len(tfidf_values)):
-            value = tfidf_values[i]
-            if value in top_25_values:
-                top_25[tfidf_tokens[i]] = value
-        sorted_top25 = sorted(top_25.items(), key=lambda x: x[1], reverse=True)
-        keywords = [k[0] for k in sorted_top25]
-        values = [k[1] for k in sorted_top25]
-        rows.append(keywords)
-        rows.append(values)
-        rows.append(['\n'])
-    csv_writer = csv.writer(open('tf-idf.csv', 'w', newline='', encoding='utf-8'))
-    csv_writer.writerows(rows)
+    # CREATE TOP 40 TERMS:
+    top_40_keywords = get_top40_keywords(tfidf_matrix, tfidf_tokens, doc_index)[:40]  # Get the 40 terms
+    # OPTIONAL #
+    # csv_writer = csv.writer(open('top-40.csv', 'w', newline='', encoding='utf-8'))  # Open and create csv file
+    # csv_writer.writerow(['WORD', 'DOCS', 'TF-IDF'])  # write header
+    # for keyword in top_40_keywords:
+    #     csv_writer.writerow([keyword[0], keyword[1][0], keyword[1][1]])  # write to row csv file
+    # OPTIONAL #
+    print('TOP 40 Terms:')
+    keywords = [key[0] for key in top_40_keywords]
+    print(keywords)
+
+    # CREATE KEYWORDS BY TOPICS:
+    # OPTIONAL #
+    # data = keywords_by_topic(tfidf_matrix, titles, doc_index)  # Get the keywords
+    # csv_writer = csv.writer(open('tf-idf.csv', 'w', newline='', encoding='utf-8'))
+    # csv_writer.writerows(data)
+    # OPTIONAL #
+
+    # 10 TERMS FROM DOMAIN KNOWLEDGE:
+    print('\nTOP 10 TERMS USING DOMAIN KNOWLEDGE')
+    terms_10 = ['joker', 'reeves', 'million', 'arkham', 'markets', 'love', 'knight', 'pattinson', 'dark', 'casting']
+    print(terms_10)
+
     # idf_set = get_idf(documents, tf_set)
     # sorted_sets = []
     # for tf_item in tf_set:
